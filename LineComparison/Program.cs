8f7f4de8154cd46367b6bfb9b4843789dfec259e
Shruthi <shruthimarri97@gmail.com>
2021-03-18 13:13:27 +0530

    [Shruthi] Add : Equals method is implemented

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,9 +7,12 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison");
-            Length line = new Length(5,8,6,10);
-            double lengthLine= line.CalculateLength();
-            Console.WriteLine("Length of a line =" + lengthLine);
+            Length lineOne = new Length(5,8,6,10);
+            double lengthOfLineOne= lineOne.CalculateLength();
+            Length lineTwo = new Length(2, 5 , 4, 8);
+            double lengthOfLineTwo = lineTwo.CalculateLength();
+            Console.WriteLine("Length of two lines are =" + lengthOfLineOne +"  " +lengthOfLineTwo);
+            Console.WriteLine(lengthOfLineOne.Equals(lengthOfLineTwo));
         }
     }
 }
